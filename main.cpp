89f6cd5fb42ce710816c9a3007a2673ea355d217
Zrordran <tibo.mar@orange.Fr>
2019-02-13 08:42:24 +0000

    debut yeux

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ const TGAColor green   = TGAColor(0, 255,   0,   255);
 const int WIDTH = 800;
 const int HEIGHT = 800;
 const int DEPTH = 255;
-int width_TEXTURE, height_TEXTURE;
+int width_TEXTURE, height_TEXTURE,width_TEXTUREY,height_TEXTUREY;
 const float light[3] = {0.,0.,1.};
 const Vector camera(0.f,0.f,3.f);
 
@@ -231,9 +231,12 @@ void drawTriangle(model m, TGAImage &image, TGAColor color, float zbuffer[]){
 
 int main(int argc, char** argv) {
     model m("../african_head.obj");
-    //model m("../diablo_pose.obj");
+    model mY("../african_head_eye_inner.obj");
     width_TEXTURE = m.imgText.get_width();
     height_TEXTURE = m.imgText.get_height();
+
+    width_TEXTUREY = mY.imgText.get_width();
+    height_TEXTUREY = mY.imgText.get_height();
 
     //Zbuffer
     float *zbuffer = new float[WIDTH*HEIGHT];
